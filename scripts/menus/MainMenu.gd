8f32ef86f132e9329ed7f3d9a569bcb3709d219c
Niel Benjamin Kho <benjokho@users.noreply.github.com>
2020-05-29 00:24:40 +0800

    added menu

--- scripts/menus/MainMenu.gd
+++ scripts/menus/MainMenu.gd
@@ -1,9 +1,28 @@
 extends CanvasModulate
 
 onready var start_button = $VBox/StartButton
+onready var player_sprite = $CharacterSprites/PlayerSprite
+onready var spirit_circle = $CharacterSprites/PlayerSprite/SpiritCircle
+
+var sprites = []
 
 func _ready():
-	pass
-
+	for i in $CharacterSprites.get_children():
+		sprites.append(i)
+	sprites.erase(player_sprite)
+	
 func _on_StartButton_pressed():
-	SceneChanger.change_scene(start_button.dest)
+	$SpellSound1.play()
+	player_sprite.play("cast_turning_spell")
+	spirit_circle.visible = true
+
+func _on_PlayerSprite_animation_finished():
+	if player_sprite.animation == "cast_turning_spell":
+		$SpellSound2.play()
+		spirit_circle.visible = false
+		for i in range (0, sprites.size()):
+			sprites[i].play("transform")
+			sprites[i].scale.x *= -1
+		player_sprite.play("idle")
+		yield(get_tree().create_timer(1.0), "timeout")
+		SceneChanger.change_scene(start_button.dest)	
